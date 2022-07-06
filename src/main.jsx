f8d11a21fcbbe7327f12d12bf291d8365a2aca19
Abhiram B.S.N <Abhiram B.S.N>
2022-07-06 17:35:08 +0530

    Routes created and base boilerplate done

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import About from "./pages/About";
+import Index from "./pages/Index";
 import { NotFound404 } from "./pages/NotFound404";
+import Team from "./pages/Team";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Index />} />
+        <Route path="about" element={<About />} />
+        <Route path="team" element={<Team />} />
         <Route path="*" element={<NotFound404 />} />
       </Routes>
     </BrowserRouter>
